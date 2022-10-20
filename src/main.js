ff7846d1290466930fd39073fa8dba2f90036ed4
Karen Cardoso <kah.card@gmail.com>
2022-10-20 12:19:29 -0300

    finalização aula 3

--- src/main.js
+++ src/main.js
@@ -66,7 +66,7 @@ const cardNumberPattern = {
     {
       mask: '0000 0000 0000 0000',
       regex:
-        /^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/,
+        /^(50(67(0[78]|1[5789]|2[012456789]|3[01234569]|4[0-7]|53|7[4-8])|9(0(0[0-9]|1[34]|2[013457]|3[0359]|4[0123568]|5[01456789]|6[012356789]|7[013]|8[123789]|9[1379])|1(0[34568]|4[6-9]|5[1245678]|8[36789])|2(2[02]|57|6[0-9]|7[1245689]|8[023456789]|9[1-6])|3(0[78]|5[78])|4(0[7-9]|1[012456789]|2[02]|5[7-9]|6[0-5]|8[45])|55[01]|636|7(2[3-8]|32|6[5-9])))|4(0117[89]|3(1274|8935)|5(1416|7(393|63[12])))|6(27780|36368|5(0(0(3[1258]|4[026]|69|7[0178])|4(0[67]|1[0-3]|2[2345689]|3[0568]|8[5-9]|9[0-9])|5(0[01346789]|1[01246789]|2[0-9]|3[0178]|5[2-9]|6[012356789]|7[01789]|8[0134679]|9[0-8])|72[0-7]|9(0[1-9]|1[0-9]|2[0128]|3[89]|4[6-9]|5[01578]|6[2356789]|7[01]))|16(5[236789]|6[025678]|7[013456789]|88)|50(0[01356789]|1[2568]|26|3[6-8]|5[1267]))))$/,
       cardtype: 'elo'
     },
     {
@@ -85,3 +85,51 @@ const cardNumberPattern = {
   }
 }
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector('#add-card')
+addButton.addEventListener('click', () => {
+  alert('Cartão adicionad!')
+})
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector('#card-holder')
+cardHolder.addEventListener('input', () => {
+  const ccHolder = document.querySelector('.cc-holder .value')
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? 'FULANO DA SILVA' : cardHolder.value
+})
+
+securityCodeMasked.on('accept', () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector('.cc-security .value')
+
+  ccSecurity.innerText = code.length === 0 ? '123' : code
+}
+
+cardNumberMasked.on('accept', () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector('.cc-number')
+
+  ccNumber.innerText = number.length === 0 ? '1234 5678 9012 3456' : number
+}
+
+expirationDateMasked.on('accept', () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector('.cc-expiration .value')
+
+  ccExpiration.innerText = date.length === 0 ? '02/32' : date
+}
